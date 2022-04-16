906995b6c8fab8009d838d9aa5c872eb461924b5
agscheer <oliver.irtenkauf@uni-konstanz.de>
2022-04-16 09:31:57 +0200

    test

---
+++ __main__.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+from library.HeliumBot import HeliumBot
+from logging import getLogger, basicConfig, INFO, WARNING
+
+logger = getLogger("HeliumBot")
+basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=INFO,
+)
+getLogger("apscheduler.scheduler").setLevel(WARNING)
+if __name__ == "__main__":
+    helium_bot = HeliumBot(dewar="Scheer 2")
+    helium_bot.init()
+    try:
+        while True:
+            sleep(3)
+    except KeyboardInterrupt:
+        helium_bot.close()
